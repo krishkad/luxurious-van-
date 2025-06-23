2d5bea82d22c92a472157fbec2692f9ce34fc8aa
krrish <krishkadam88@gmail.com>
2025-06-23 16:04:22 +0530

    second commit

--- src/components/PackagesSection.tsx
+++ src/components/PackagesSection.tsx
@@ -1,7 +1,6 @@
 "use client"
-import React from 'react';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 
 const PackagesSection = () => {
   const packages = [
