4f63b6968d5f45bd1c2265f89a3f14e78a4b9833
krrish <krishkadam88@gmail.com>
2025-06-23 16:27:17 +0530

    hero image changed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
       <div 
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: `url('https://images.unsplash.com/photo-1482938289607-e9573fc25ebb?ixlib=rb-4.0.3&auto=format&fit=crop&w=2000&q=80')`
+          backgroundImage: `url('https://plus.unsplash.com/premium_photo-1682678368748-3e9ffcda4d79?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`
         }}
       />
       
